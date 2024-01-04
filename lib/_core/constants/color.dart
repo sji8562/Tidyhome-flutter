4c83a6287914a14d5aed7e622f949e313303d8b2
Jys <onlys2545@naver.com>
2024-01-04 10:45:43 +0900

    초기 세팅 일부 수정

--- lib/_core/constants/color.dart
+++ lib/_core/constants/color.dart
@@ -21,3 +21,20 @@ const MaterialColor kPrimaryColor = MaterialColor(
     900: Color(0XFF2D403A),
   },
 );
+
+const primaryColor = Color(0xff367DF7);
+const primaryColor02 = Color(0xffF0F7FF);
+const primaryColor03 = Color(0xff4b89e7);
+const pointColor = Color(0xffEBE1F2);
+const accentColor = Color(0xffFC602F);
+const reviewColor = Color(0xffFCB81B);
+const validationColor = Color(0xffC64F48);
+const basicColorB1 = Color(0xff111111);
+const basicColorB3 = Color(0xff333333);
+const basicColorB5 = Color(0xff555555);
+const basicColorB7 = Color(0xff777777);
+const basicColorB9 = Color(0xff999999);
+const formColor = Color(0xffcccccc);
+const disableColor = Color(0xffc5c5c5);
+const bgAndLineColor = Color(0xffeeeeee);
+const basicColorW = Color(0xffffffff);
