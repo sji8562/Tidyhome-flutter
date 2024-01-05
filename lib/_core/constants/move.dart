0d709f339d87c03f73717ff6250d223c6fd13237
aSpring712 <rnjsqhal2001@naver.com>
2024-01-05 17:49:30 +0900

    서비스 상세정보, 문의사항 페이지

--- lib/_core/constants/move.dart
+++ lib/_core/constants/move.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:toyproject/ui/pages/customer_page/customer_page.dart';
 import 'package:toyproject/ui/pages/reservation_page/find_address_page.dart';
+import 'package:toyproject/ui/pages/service_detail_page/applience_page.dart';
+import 'package:toyproject/ui/pages/service_detail_page/movement_page.dart';
 import 'package:toyproject/ui/pages/splash_page/splash_page.dart';
 import '../../ui/pages/join_page/join_page.dart';
 import '../../ui/pages/service_detail_page/housekeeper_page.dart';
+import '../../ui/pages/service_detail_page/office_page.dart';
 import '../../ui/pages/start_page/start_page.dart';
 import 'package:toyproject/ui/pages/reservation_page/choice_address_page.dart';
 import 'package:toyproject/ui/pages/reservation_page/reservation_page.dart';
@@ -30,6 +34,10 @@ class Move {
   static String ChoiceAddressPage = '/choice-address';
   static String FindAddressPage = '/find-address';
   static String HouseKeeperPage = '/housekeeper';
+  static String OfficePage = '/office';
+  static String MovementPage = '/movement';
+  static String AppliencePage = '/applience';
+  static String CustomerPage = '/customer';
 }
 
 Map<String, Widget Function(BuildContext)> getRouters() {
@@ -44,6 +52,10 @@ Map<String, Widget Function(BuildContext)> getRouters() {
     Move.ChoiceAddressPage: (context) => ChoiceAddressPage(),
     Move.FindAddressPage: (context) => FindAddressPage(),
     Move.HouseKeeperPage: (context) => HouseKeeperPage(),
+    Move.OfficePage: (context) => OfficePage(),
+    Move.MovementPage: (context) => MovementPage(),
+    Move.AppliencePage: (context) => AppliencePage(),
+    Move.CustomerPage: (context) => CustomerPage(),
 
     // Move.MainPage: (context) => MainPage(),
     // Move.LoginPage: (context) => LoginPage(),
