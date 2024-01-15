1341bdfa2721942d5bf141e86644fa6ca8f680eb
Jys <onlys2545@naver.com>
2024-01-15 14:30:23 +0900

    예약 취소 페이지 완료

--- lib/ui/pages/reservation_page/reservation_cancle_page/reservation_cancel_page.dart
+++ lib/ui/pages/reservation_page/reservation_cancle_page/reservation_cancel_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:toyproject/ui/pages/reservation_page/reservation_cancle_page/widget/reservation_cancel_page_body.dart';
+import 'package:toyproject/ui/widget/arrow_app_bar.dart';
 
 class ReservationCancelPage extends StatelessWidget {
   const ReservationCancelPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: ArrowAppBar(leading: Icons.keyboard_backspace, title: '일정 취소하기'),
+      body: const ReservationCancelPageBody(),
+    );
   }
 }
