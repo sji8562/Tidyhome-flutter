5873872931dcbf1700d1257153f05ed102bd6c01
aSpring712 <rnjsqhal2001@naver.com>
2024-01-15 17:48:20 +0900

    [권보미] 회원탈퇴 연결

--- lib/ui/pages/customer_page/account_closure_page/account_closure_page.dart
+++ lib/ui/pages/customer_page/account_closure_page/account_closure_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:toyproject/_core/constants/color.dart';
 import 'package:toyproject/_core/constants/move.dart';
+import 'package:toyproject/ui/pages/customer_page/account_closure_page/account_closure_page_view_model.dart';
 import 'package:toyproject/ui/pages/reservation_page/widget/reservation_tab.dart';
 import 'package:toyproject/ui/widget/arrow_app_bar.dart';
 import 'package:toyproject/ui/widget/button/soft_color_button.dart';
@@ -10,11 +12,11 @@ import 'package:toyproject/ui/widget/button/soft_color_red_button.dart';
 import '../widget/icon_and_title.dart';
 import '../widget/title_only.dart';
 
-class AccountClosurePage extends StatelessWidget {
+class AccountClosurePage extends ConsumerWidget {
   const AccountClosurePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
         appBar: ArrowAppBar(leading: Icons.keyboard_backspace, title: ''),
         body: SingleChildScrollView(
@@ -41,28 +43,27 @@ class AccountClosurePage extends StatelessWidget {
                           '- 적립금, 예치금, 쿠폰 등 모든 혜택이 사라집니다.\n'
                       ),
                     ),
-                    Divider(
-                      color: disableColor,
-                      height: 20
-                    ),
-                    TextField(
-                      maxLines: null, // 다중 행으로 설정하려면 null 또는 원하는 줄 수를 지정
-                      maxLength: 300,
-                      decoration: InputDecoration(
-                        hintText: '내용을 입력해주세요',
-                        hintStyle: TextStyle(color: disableColor),
-                        border: InputBorder.none, // 밑줄 없애기
-                      ),
-                      onChanged: (text) {
-                        // 텍스트가 변경될 때 실행되는 콜백 함수
-                        print('Typed text: $text');
-                      },
-                    ),
-                    Divider(
-                        color: disableColor,
-                        height: 20
-                    ),
-                    SoftColorRedButton(text: '탈퇴 요청하기', funPageRoute: () {}),
+                    // Divider(
+                    //   color: disableColor,
+                    //   height: 20
+                    // ),
+                    // TextField(
+                    //   maxLines: null, // 다중 행으로 설정하려면 null 또는 원하는 줄 수를 지정
+                    //   maxLength: 300,
+                    //   decoration: InputDecoration(
+                    //     hintText: '내용을 입력해주세요',
+                    //     hintStyle: TextStyle(color: disableColor),
+                    //     border: InputBorder.none, // 밑줄 없애기
+                    //   ),
+                    //   onChanged: (text) {
+                    //     // 텍스트가 변경될 때 실행되는 콜백 함수
+                    //     print('Typed text: $text');
+                    //   },
+                    // ),
+                    const SizedBox(height: 30,),
+                    SoftColorRedButton(text: '탈퇴 요청하기', funPageRoute: () {
+                      ref.read(accountClosureProvider).accountClose();
+                    }),
                   ],
                 )
             ),
